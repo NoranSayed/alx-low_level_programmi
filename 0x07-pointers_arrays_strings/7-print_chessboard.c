668e5b4ab00195b231d02018e494240f3c736138
NoranSayed <119365828+NoranSayed@users.noreply.github.com>
2023-04-05 03:18:24 +0200

    Update 7-print_chessboard.c

--- 0x07-pointers_arrays_strings/7-print_chessboard.c
+++ 0x07-pointers_arrays_strings/7-print_chessboard.c
@@ -2,18 +2,18 @@
 
 /**
  * print_chessboard - prints chessboard.
- * @a: matrix
+ * @f: matrix
 */
 
-void print_chessboard(char (*a)[8])
+void print_chessboard(char (*f)[8])
 {
 	int i, j;
 
-	for (i = 0; i < 8; i++)
+	for (j = 0; j < 8; j++)
 	{
-		for (j = 0; j < 8; j++)
+		for (i = 0; i < 8; i++)
 		{
-			_putchar(*(*(i + a) + j));
+			_putchar(*(*(j + f) + i));
 		}
 	_putchar('\n');
 	}
